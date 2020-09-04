b66bce5dac8d4f8d6135c2364dbd76275c6b1582
Mauricio <mauriciobastiani@outlook.com>
2020-09-04 10:49:02 -0300

    node packages update

--- Shared/Database.js
+++ Shared/Database.js
@@ -1,10 +1,10 @@
 const Mongoose = require('mongoose');
 
 //module.exports.connect = function () {
-  Mongoose.connect(process.env["ENV_MONGODB"], {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+Mongoose.connect(process.env["ENV_MONGODB"], {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 //}
